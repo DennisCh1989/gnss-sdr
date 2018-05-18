ce7172deed2ff36c9aab1e087a493530604640d3
DennisCh1989 <chuprakovden@yandex.ru>
2018-05-18 09:16:36 +0000

    catch error in volk kernel

--- src/algorithms/tracking/libs/recovery_kernel.cc
+++ src/algorithms/tracking/libs/recovery_kernel.cc
@@ -20,9 +20,9 @@ void recovery_kernel::gen(  float init_phase, float incm_phase,
 	    
    volk_32fc_s32fc_multiply_32fc(out, in, d_ampl_est*(1-REC_FAC)/ real(norm), chunk_length);
 
-   lv_32fc_t incm = std::exp(lv_32fc_t(0,incm_phase));
+   lv_32fc_t phase = std::exp(lv_32fc_t(0,incm_phase));
 	    
-   volk_32fc_s32fc_x2_rotator_32fc(out, out, std::exp(lv_32fc_t(0,init_phase)), incm, chunk_length); 
+   volk_32fc_s32fc_x2_rotator_32fc(out, out, std::exp(lv_32fc_t(0,incm_phase)), &phase, chunk_length); 
 }
 
 
