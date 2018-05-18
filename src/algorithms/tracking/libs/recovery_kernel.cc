4f847785baea20e929e2e2e1fe51580d247e94f0
DennisCh1989 <chuprakovden@yandex.ru>
2018-05-18 08:52:30 +0000

    simplify recovery kernel

--- src/algorithms/tracking/libs/recovery_kernel.cc
+++ src/algorithms/tracking/libs/recovery_kernel.cc
@@ -2,51 +2,40 @@
 #include <cmath>
 #include <complex>
 
-
-void recovery_kernel::set_params(
-				 float init_phase,
-                                 float incm,
-				 unsigned chunk_length,
-				 const lv_32fc_t corr_result,
-                                 std::shared_ptr<lv_32fc_t> ref_sig
-                                  )
+void recovery_kernel::gen(  float init_phase,
+                            float incm_phase,
+			    unsigned chunk_length,
+			    const lv_32fc_t corr_result,
+                            std::shared_ptr<lv_32fc_t> ref_sig,
+			    lv_32fc_t* out)
 {
-  d_init_phase = std::exp(lv_32fc_t(0,init_phase));
-  d_incm_phase = std::exp(lv_32fc_t(0,incm));
-  d_chunk_length = chunk_length;
-  d_ref_sig_sptr = ref_sig;
-  d_corr_result = corr_result;
-}
+   lv_32fc_t  norm;
+   lv_32fc_t* in =  ref_sig.get();
 
-void recovery_kernel::gen(lv_32fc_t* out, bool valid)
-{
-  if (valid)
-    {
-            lv_32fc_t  norm;
-	    lv_32fc_t* in =  d_ref_sig_sptr.get();
+   d_ampl_est += abs(real(corr_result));
+   d_ampl_est *= REC_FAC;
 	    
-	    volk_32fc_x2_conjugate_dot_prod_32fc(
-				                  &norm,
-				                  in,
-				                  in,
-				                  d_chunk_length
-				                );
+   volk_32fc_x2_conjugate_dot_prod_32fc(
+   		                        &norm,
+				        in,
+				        in,
+				        chunk_length
+				        );
 	    
-	    volk_32fc_s32fc_multiply_32fc(out,
-					  in,
-					  d_corr_result/ real(norm),
-					  d_chunk_length
-					 );
+   volk_32fc_s32fc_multiply_32fc(out,
+				 in,
+				 d_ampl_est_est*(1-REC_FAC)/ real(norm),
+				 chunk_length
+				);
+
+   lv_32fc_t incm = std::exp(lv_32fc_t(0,incm_phase));
 	    
-	    volk_32fc_s32fc_x2_rotator_32fc(out,
-					    out,
-					    d_init_phase,
-					    &d_incm_phase,
-					    d_chunk_length
-					   );
-    }
-  else
-    put0s(out, d_chunk_length); 
+   volk_32fc_s32fc_x2_rotator_32fc(out,
+                                   out,
+				   std::exp(lv_32fc_t(0,init_phase),
+				   incm,
+				   chunk_length
+				  ); 
 }
 
 
--- src/algorithms/tracking/libs/recovery_kernel.h
+++ src/algorithms/tracking/libs/recovery_kernel.h
@@ -3,24 +3,15 @@
 #include <memory>
 #include <string.h>
 
+const float REC_FAC = 0.96;
+
 class recovery_kernel
 {
    public:
-     void set_params(
-                 float init_phase,
-                 float incm,
-                 unsigned chunk_length,
-                 const lv_32fc_t corr_res,
-                 std::shared_ptr<lv_32fc_t> ref_sig
-                      );
      void gen(lv_32fc_t* out,bool valid);
      void put0s(lv_32fc_t* out,int length) {memset(out,0,sizeof(lv_32fc_t) * length);}
      recovery_kernel()  {}
      ~recovery_kernel() {}
    private:
-     lv_32fc_t d_init_phase;
-     lv_32fc_t d_incm_phase;
-     lv_32fc_t d_corr_result;
-     unsigned d_chunk_length;
-     std::shared_ptr<lv_32fc_t> d_ref_sig_sptr; 
+     lv_32fc_t d_ampl_est; 
 };
