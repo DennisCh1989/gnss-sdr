eca1adacdd848809fba9512a887f94e5e7d3d625
DennisCh1989 <chuprakovden@yandex.ru>
2018-05-18 09:24:13 +0000

    error in last volk kernel in recovery kernel

--- src/algorithms/tracking/libs/recovery_kernel.cc
+++ src/algorithms/tracking/libs/recovery_kernel.cc
@@ -20,7 +20,7 @@ void recovery_kernel::gen(  float init_phase, float incm_phase,
 	    
    volk_32fc_s32fc_multiply_32fc(out, in, d_ampl_est*(1-REC_FAC)/ real(norm), chunk_length);
 
-   lv_32fc_t phase = std::exp(lv_32fc_t(0,incm_phase));
+   lv_32fc_t phase = std::exp(lv_32fc_t(0,init_phase));
 	    
    volk_32fc_s32fc_x2_rotator_32fc(out, out, std::exp(lv_32fc_t(0,incm_phase)), &phase, chunk_length); 
 }
