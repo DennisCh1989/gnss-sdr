2847f24a28cd803e9083e28386267aa43bff0fa8
DennisCh1989 <chuprakovden@yandex.ru>
2018-05-21 23:48:58 +0300

    correct subframe3 index

--- src/algorithms/tracking/libs/recovery_kernel.cc
+++ src/algorithms/tracking/libs/recovery_kernel.cc
@@ -1,6 +1,7 @@
 #include "recovery_kernel.h"
 #include <cmath>
 #include <complex>
+#include "GPS_L1_CA.h"
 
 
 recovery_kernel rec_ker;
@@ -15,22 +16,20 @@ void    recovery_kernel::gen(  float init_phase, float incm_phase,
    lv_32fc_t  norm;
 
    if (symb_id <24000)
-     {
-       d_ampl_est =0;
-     }
+     d_ampl_est =0;
    else
      {
-       d_ampl_est += real(corr_result) *subframe3.get()[symb_id]; 
+       d_ampl_est += real(corr_result) *subframe3.get()[symb_id/GPS_CA_TELEMETRY_SYMBOLS_PER_BIT]; 
        d_ampl_est *= REC_FAC;
      }
   
-   if (symb_id >= 27500 and symb_id <30000)
+   if (symb_id >= 27500)
      {
         volk_32fc_conjugate_32fc(out, ref_sig.get(),chunk_length);
 	    
         volk_32fc_x2_conjugate_dot_prod_32fc(&norm, out, out, chunk_length);
 	    
-        volk_32fc_s32fc_multiply_32fc(out, out,subframe3.get()[symb_id] * d_ampl_est*(1-REC_FAC)/ real(norm), chunk_length);
+        volk_32fc_s32fc_multiply_32fc(out, out,subframe3.get()[symb_id/GPS_CA_TELEMETRY_SYMBOLS_PER_BIT] * d_ampl_est*(1-REC_FAC)/ real(norm), chunk_length);
 
         lv_32fc_t phase = std::exp(lv_32fc_t(0,init_phase));
 	    
