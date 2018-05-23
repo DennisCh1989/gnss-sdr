9dc93cd2ae0f759c5043154d366d95f4bb74280b
DennisCh1989 <chuprakovden@yandex.ru>
2018-05-23 10:26:12 +0000

    rec ker works rightly

--- src/algorithms/tracking/libs/recovery_kernel.cc
+++ src/algorithms/tracking/libs/recovery_kernel.cc
@@ -1,12 +1,15 @@
 #include "recovery_kernel.h"
 #include <cmath>
 #include <complex>
+#include <cinttypes>
 #include "GPS_L1_CA.h"
+
+const int START_EST = 4; //where we start to pass tags
 
 
 recovery_kernel rec_ker;
 
-void    recovery_kernel::gen(  float init_phase, float incm_phase,
+bool    recovery_kernel::gen(  float init_phase, float incm_phase,
 			       unsigned chunk_length,
 			       const lv_32fc_t corr_result,
                                std::shared_ptr<lv_32fc_t> ref_sig,
@@ -15,21 +18,54 @@ void    recovery_kernel::gen(  float init_phase, float incm_phase,
 {
    lv_32fc_t  norm;
 
-   d_ampl_est += real(corr_result) *subframe3.get()[symb_id/GPS_CA_TELEMETRY_SYMBOLS_PER_BIT]; 
-   d_ampl_est *= REC_FAC;
+   symb_id -= GPS_SUBFRAME_MS*(GPS_SUBFRAME_NUM-1); // GPS_SUBFRAME_MS -- also equls symbols per subframe
+
+   if (symb_id >= 0)
+     {
+       d_ampl_est += real(corr_result) *subframe3[symb_id/GPS_CA_TELEMETRY_SYMBOLS_PER_BIT]; 
+       d_ampl_est *= REC_FAC;
+     }
   
-   if (symb_id >= 28000) 
+   if (symb_id >= GPS_CA_TELEMETRY_RATE_SYMBOLS_SECOND *  START_EST) 
      {
         volk_32fc_conjugate_32fc(out, ref_sig.get(),chunk_length);
 	    
         volk_32fc_x2_conjugate_dot_prod_32fc(&norm, out, out, chunk_length);
 	    
-        volk_32fc_s32fc_multiply_32fc(out, out,subframe3.get()[symb_id/GPS_CA_TELEMETRY_SYMBOLS_PER_BIT] * d_ampl_est*(1-REC_FAC)/ real(norm), chunk_length);
+        volk_32fc_s32fc_multiply_32fc(out, out,subframe3[symb_id/GPS_CA_TELEMETRY_SYMBOLS_PER_BIT] * d_ampl_est*(1-REC_FAC)/ real(norm), chunk_length);
 
         lv_32fc_t phase = std::exp(lv_32fc_t(0,init_phase));
 	    
         volk_32fc_s32fc_x2_rotator_32fc(out, out, std::exp(lv_32fc_t(0,incm_phase)), &phase, chunk_length);
+
+	return true; // let's put tags in out num 1
      }
+
+   return false;
+}
+
+void recovery_kernel :: get_subframe (char * subframe)
+{
+  int pos =0;
+  int cnt =0;
+  for (int i =0;i < GPS_SUBFRAME_LENGTH;i++)
+    {
+      uint8_t byte = subframe[i];
+      for (int j =0;j < 8;j++)
+	{
+	  int pos_in_4bytes = pos % 32;
+	  if (pos_in_4bytes >=2)
+	    {
+	      if (byte & 0x80)
+		subframe3[cnt] = sign_flag;
+	      else
+		subframe3[cnt] = -sign_flag;
+	      cnt++;
+	    }
+	  byte <<=1;
+	  pos++;
+	}
+    }
 }
 
 
--- src/algorithms/tracking/libs/recovery_kernel.h
+++ src/algorithms/tracking/libs/recovery_kernel.h
@@ -8,17 +8,20 @@ const float REC_FAC = 0.96;
 class recovery_kernel
 {
    public:
-     void gen( float init_phase, float incm_phase,
+     bool gen( float init_phase, float incm_phase,
                unsigned chunk_length,
 	       const lv_32fc_t corr_result,
                std::shared_ptr<lv_32fc_t> ref_sig,
 	       lv_32fc_t* out,
                int symb_id);
      void put0s(lv_32fc_t* out,int length) {memset(out,0,sizeof(lv_32fc_t) * length);}
-     recovery_kernel()  {subframe3 = std::shared_ptr<float> (new float[1600]);}
-     ~recovery_kernel() {}
-     std::shared_ptr<float> subframe3; 
+     recovery_kernel() {}
+     ~recovery_kernel() {} 
+     void get_subframe(char * gps_frame);
+     void put_sign(float sign) {(sign>0) ? sign_flag = 1 : sign_flag = -1;}
    private:
+     float subframe3[2000];
      lv_32fc_t d_ampl_est; 
+     float sign_flag;
      int symb_id;
 };
