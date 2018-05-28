8b022237ef824d5de6a07337cb6da02f9cf5496d
DennisCh1989 <chuprakovden@yandex.ru>
2018-05-28 18:28:40 +0300

    add impl of passive radar

---
+++ src/algorithms/passive_radar/gnuradio_blocks/passive_radar_cc.h
@@ -0,0 +1,87 @@
+/*!
+ * \file gps_l1_ca_dll_pll_tracking.h
+ * \brief  Interface of an adapter of a DLL+PLL tracking loop block
+ * for GPS L1 C/A to a TrackingInterface
+ * \author Carlos Aviles, 2010. carlos.avilesr(at)googlemail.com
+ *         Javier Arribas, 2011. jarribas(at)cttc.es
+ *
+ * Code DLL + carrier PLL according to the algorithms described in:
+ * K.Borre, D.M.Akos, N.Bertelsen, P.Rinder, and S.H.Jensen,
+ * A Software-Defined GPS and Galileo Receiver. A Single-Frequency
+ * Approach, Birkha user, 2007
+ *
+ * -------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2015  (see AUTHORS file for a list of contributors)
+ *
+ * GNSS-SDR is a software defined Global Navigation
+ *          Satellite Systems receiver
+ *
+ * This file is part of GNSS-SDR.
+ *
+ * GNSS-SDR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GNSS-SDR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNSS-SDR. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------
+ */
+
+#ifndef PASSIVE_RADAR_H_
+#define PASSIVE_RADAR_H_
+
+#include <string>
+#include "passive_radar_interface.h"
+#include "passive_radar_cc.h"
+
+
+class ConfigurationInterface;
+
+class PassiveRadar : public PassiveRadarInterface
+{
+public:
+    PassiveRadar(ConfigurationInterface* configuration,
+            std::string role,
+            unsigned int in_streams,
+            unsigned int out_streams);
+
+    virtual ~PassiveRadar();
+
+    std::string role()
+    {
+        return role_;
+    }
+
+    //! Returns "PassiveRadar"
+    std::string implementation()
+    {
+        return "PassiveRadar";
+    }
+
+    size_t item_size()
+    {
+        return item_size_;
+    }
+
+    void connect(gr::top_block_sptr top_block);
+    void disconnect(gr::top_block_sptr top_block);
+    gr::basic_block_sptr get_left_block();
+    gr::basic_block_sptr get_right_block();
+
+private:
+    passive_radar_cc_sptr passive_radar_;
+    size_t item_size_;
+    std::string role_;
+    unsigned int in_streams_;
+    unsigned int out_streams_;
+};
+
+#endif // PASSIVE_RADAR_H_
