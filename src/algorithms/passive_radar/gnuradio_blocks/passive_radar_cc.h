bd1e0a8170e2c5b5660b1d34f06aaa62e23f6461
DennisCh1989 <chuprakovden@yandex.ru>
2018-05-29 21:33:34 +0300

    modified passive radar impl

--- src/algorithms/passive_radar/gnuradio_blocks/passive_radar_cc.h
+++ src/algorithms/passive_radar/gnuradio_blocks/passive_radar_cc.h
@@ -1,87 +1,61 @@
-/*!
- * \file gps_l1_ca_dll_pll_tracking.h
- * \brief  Interface of an adapter of a DLL+PLL tracking loop block
- * for GPS L1 C/A to a TrackingInterface
- * \author Carlos Aviles, 2010. carlos.avilesr(at)googlemail.com
- *         Javier Arribas, 2011. jarribas(at)cttc.es
- *
- * Code DLL + carrier PLL according to the algorithms described in:
- * K.Borre, D.M.Akos, N.Bertelsen, P.Rinder, and S.H.Jensen,
- * A Software-Defined GPS and Galileo Receiver. A Single-Frequency
- * Approach, Birkha user, 2007
- *
- * -------------------------------------------------------------------------
- *
- * Copyright (C) 2010-2015  (see AUTHORS file for a list of contributors)
- *
- * GNSS-SDR is a software defined Global Navigation
- *          Satellite Systems receiver
- *
- * This file is part of GNSS-SDR.
- *
- * GNSS-SDR is free software: you can redistribute it and/or modify
+/* -*- c++ -*- */
+/* 
+ * Copyright 2018 Dennis Chuprakov.
+ * 
+ * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * GNSS-SDR is distributed in the hope that it will be useful,
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
- * along with GNSS-SDR. If not, see <http://www.gnu.org/licenses/>.
- *
- * -------------------------------------------------------------------------
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
  */
 
-#ifndef PASSIVE_RADAR_H_
-#define PASSIVE_RADAR_H_
+#ifndef INCLUDED_PASSIVE_RADAR_PASSIVE_RADAR_IMPL_H
+#define INCLUDED_PASSIVE_RADAR_PASSIVE_RADAR_IMPL_H
 
-#include <string>
-#include "passive_radar_interface.h"
-#include "passive_radar_cc.h"
+#include <passive_radar/passive_radar.h>
 
+typedef boost::shared_ptr<passive_radar_cc>
+               passive_radar_cc_sptr;
 
-class ConfigurationInterface;
+passive_radar_cc_sptr
+make_passive_radar_cc(
+                        float fs_in,
+                        float duration,
+                        unsigned int sources_count,
+                        unsigned int channles_count,
+                        unsigned int vector_length
+                       );
 
-class PassiveRadar : public PassiveRadarInterface
-{
-public:
-    Passive_Radar(ConfigurationInterface* configuration,
-            std::string role,
-            unsigned int in_streams,
-            unsigned int out_streams);
-
-    virtual ~PassiveRadar();
-
-    std::string role()
+    class passive_radar_cc : public passive_radar
     {
-        return role_;
-    }
+     private:
+      friend passive_radar_cc_sptr make_passive_radar_cc(
+                                                          float fs_in,
+                                                          float duration,
+                                                          unsigned int sources_count,
+                                                          unsigned int channles_count,
+                                                          unsigned int vector_length
+                                                           );
 
-    //! Returns "PassiveRadar"
-    std::string implementation()
-    {
-        return "PassiveRadar";
-    }
+     public:
+      passive_radar_cc();
+      ~passive_radar_cc();
 
-    size_t item_size()
-    {
-        return item_size_;
-    }
+      // Where all the action really happens
+      int work(int noutput_items,
+         gr_vector_const_void_star &input_items,
+         gr_vector_void_star &output_items);
+    };
 
-    void connect(gr::top_block_sptr top_block);
-    void disconnect(gr::top_block_sptr top_block);
-    gr::basic_block_sptr get_left_block();
-    gr::basic_block_sptr get_right_block();
 
-private:
-    passive_radar_cc_sptr passive_radar_;
-    size_t item_size_;
-    std::string role_;
-    unsigned int in_streams_;
-    unsigned int out_streams_;
-};
+#endif /* INCLUDED_PASSIVE_RADAR_PASSIVE_RADAR_IMPL_H */
 
-#endif // PASSIVE_RADAR_H_
---
+++ src/algorithms/passive_radar/gnuradio_blocks/passive_radar_сс.cc
@@ -0,0 +1,82 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2018 <+YOU OR YOUR COMPANY+>.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <gnuradio/io_signature.h>
+#include "passive_radar_cc.h"
+
+
+    passive_radar_cc_sptr make_passive_radar_cc( 
+                                                  float fs_in,
+                                                  float duration,
+                                                  unsigned int sources_count,
+                                                  unsigned int channles_count,
+                                                  unsigned int vector_length
+                                                    )
+    {
+      return passive_radar_cc_sptr (new passive_radar_cc(
+                                                         float fs_in,
+                                                         float duration,
+                                                         unsigned int sources_count,
+                                                         unsigned int channles_count,
+                                                         unsigned int vector_length
+                                                           ));
+    }
+
+    /*
+     * The private constructor
+     */
+    passive_radar_cc::passive_radar_cc(
+                                       float fs_in,
+                                       float duration,
+                                       unsigned int sources_count,
+                                       unsigned int channles_count ,
+                                       unsigned int vector_length
+                                        )
+      : gr::sync_block("passive_radar",
+              gr::io_signature::make(1, -1, sizeof(gr_complex)),
+              gr::io_signature::make(0, 0, 0))
+    {}
+
+    /*
+     * Our virtual destructor.
+     */
+    passive_radar_cc::~passive_radar_cc()
+    {
+    }
+
+    int
+    passive_radar_cc::work(int noutput_items,
+        gr_vector_const_void_star &input_items,
+        gr_vector_void_star &output_items)
+    {
+      const gr_complex  * in = (const  gr_complex *) input_items[0];
+      
+
+      // Do <+signal processing+>
+
+      // Tell runtime system how many output items we produced.
+      return noutput_items;
+    }
+
+
