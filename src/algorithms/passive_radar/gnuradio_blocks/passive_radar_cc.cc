52313eeb38c5db69df3fd534ed0782af5c306797
DennisCh1989 <chuprakovden@yandex.ru>
2018-06-07 16:12:21 +0000

    refactoring passive_radar code

--- src/algorithms/passive_radar/gnuradio_blocks/passive_radar_cc.cc
+++ src/algorithms/passive_radar/gnuradio_blocks/passive_radar_cc.cc
@@ -77,14 +77,12 @@
       d_run_detector = false;
       d_doppler_range = -5000;
       d_doppler_step = (1/duration)*DOPPLER_DEFINITION;
-      d_threshold =static_cast<unsigned int> (duration*GPS_CA_TELEMETRY_RATE_BITS_SECOND*STRONG_BITS_FREQ);
       d_resampling_doppler_dist =MAX_TIME_SHIFT/(GPS_L1_CA_CODE_RATE_HZ*duration) *GPS_L1_FREQ_HZ;
       std::vector<float> taps;
       d_resamp = new gr::filter::kernel::pfb_arb_resampler_ccf(1, taps, 1);
       float max_chip_rate_err = d_doppler_range/GPS_L1_FREQ_HZ; 
       d_resampled_input = std::shared_ptr<gr_complex>(new gr_complex[static_cast<unsigned int>(d_conv_chunk*(1+max_chip_rate_err*2))]);
       d_freq_shift_input = std::shared_ptr<gr_complex>(new gr_complex[static_cast<unsigned int>(d_conv_chunk*(1+max_chip_rate_err*2))]); 
-      d_reliable_channel_flags.resize(d_channels_count);
       unsigned int d_vector_length = static_cast<unsigned int> (fs_in / (GPS_L1_CA_CODE_RATE_HZ / GPS_L1_CA_CODE_LENGTH_CHIPS))*GPS_CA_TELEMETRY_SYMBOLS_PER_BIT*2;
       set_history(d_vector_length);
     }
@@ -96,111 +94,63 @@
     {
     }
 
-    void passive_radar_cc::detector(gr_vector_const_void_star input_items)
-    {
-      for (unsigned int  conditioner_id =0; conditioner_id < d_conditioners_count; conditioner_id++)
-	{
+    std::vector<bool> reliable_channel;
 
-          gr_complex *in = (gr_complex*) input_items[conditioner_id];
-
-	  for (unsigned ch =0;ch < d_channels_count;ch++)
-	    {
-	      if (conditioner_id == d_IDs[ch]  and d_reliable_channel_flags[ch])
-		{
-		  float resampling_last_doppler = 0;
-		  
-		  // here perform direct FFT for input_items[ch + d_conditioners_count]
-		  
-		  for (float doppler = -d_doppler_range;doppler < d_doppler_range;doppler+=d_doppler_step) 
-		    {
-
-		      if (doppler -resampling_last_doppler >d_resampling_doppler_dist or doppler == - d_doppler_range)
-			{
-			  resampling_last_doppler = doppler;
-			  
-			  float chip_rate_err =doppler / GPS_L1_FREQ_HZ;
-
-			  d_resamp -> set_phase(0);
-			  d_resamp -> set_rate(1+chip_rate_err);
-			  int nitems_read;
-			  int processed = d_resamp->filter(d_resampled_input.get(), in, d_conv_chunk, nitems_read);
-
-			  for (int i = processed;i < d_conv_chunk;i++)
-			    {
-			      d_resampled_input.get()[i] =0;
-			    }
-			}
-
-		      memcpy(d_freq_shift_input.get(), d_resampled_input.get(), sizeof(gr_complex)*d_conv_chunk);
-		      // here correct  d_freq_shift_doppler by doppler shift
-		      // here perform  direct FFT for d_freq_shift_doppler
-		      // here perform  element-wise production d_freq_shift_doppler with FFT(input_items[ch])
-		      // here perform  Inverse FFT
-		      // here get magnitudes
-		    }
-		}
-	    }
-	}
-      
-      d_run_detector = false;
-    }
-
-
-    int
-    passive_radar_cc::work(int noutput_items __attribute__ ((unused)),
+    int passive_radar_cc::work(int noutput_items __attribute__ ((unused)),
         gr_vector_const_void_star &input_items,
         gr_vector_void_star &output_items __attribute__ ((unused)))
     {
 
-      if (d_run_detector == false)
-	{
-	   d_run_detector = true;
+      if (d_run_detector) return d_conv_chunk;
+      d_run_detector = true;
+      std::thread([this,input_items]
+		  {
+		    for (unsigned ch =0;ch < d_channels_count;ch++)
+		      {
+			if (!reliable_channel[ch]) continue;
 
-	   unsigned int reliable_channels =0;
+			gr_complex *in = (gr_complex*) input_items[d_IDs[ch]];
 
-	   for (unsigned channel_id = 0; channel_id < d_channels_count; channel_id++)
-	     {
-	       std::vector<gr::tag_t> bits;
+			volk_32f_x2_subtract_32f(
+						 (float*) input_items[d_IDs[ch]],
+						 (float*) input_items[d_IDs[ch]],
+						 (float*) input_items[ch+d_conditioners_count],
+						 d_conv_chunk*sizeof(gr_complex)/sizeof(float)
+						 );
+		  
+			// here perform direct FFT for input_items[ch + d_conditioners_count]
 
-               d_reliable_channel_flags[channel_id] = false;
+			float chip_rate_err=0;
+		  
+			for (float doppler = -d_doppler_range;doppler < d_doppler_range;doppler+=d_doppler_step) 
+			  {
 
-	       get_tags_in_window(
-				  bits,
-				  channel_id+d_conditioners_count,
-				  0,
-				  d_conv_chunk ,
-				  pmt::mp("reliable symbol")
-				  );
-	      
-	       if (bits.size() >  d_threshold)
-		 {
-                   d_reliable_channel_flags[channel_id] = true;
-		   reliable_channels++;
+			    if (abs(doppler / GPS_L1_FREQ_HZ - chip_rate_err) > d_resampling_doppler_dist/GPS_L1_FREQ_HZ)
+			      {
+				chip_rate_err =doppler / GPS_L1_FREQ_HZ;
 
-		   unsigned int conditioner_id_for_chan = d_IDs[channel_id];
-		   
-		   for (unsigned int i =0;i < bits.size() ;i++)
-		     {
-		       unsigned int bit_pos = bits[i].offset - nitems_read(0);
-		       float *cVec =  &((float*) &input_items[conditioner_id_for_chan])[bit_pos*2];
-		       float *aVec =  cVec; 
-		       float *bVec =  &((float*) &input_items[channel_id+d_conditioners_count])[bit_pos*2];
-		       unsigned int bit_samples_length = pmt::to_long(bits[i].value);
-		       volk_32f_x2_subtract_32f(cVec,aVec,bVec,bit_samples_length*sizeof(gr_complex)/sizeof(float));
-		     }
-		 }
-	     }
+				d_resamp -> set_phase(0);
+				d_resamp -> set_rate(1+chip_rate_err);
+				int nitems_read;
+				int processed = d_resamp->filter(d_resampled_input.get(), in, d_conv_chunk, nitems_read);
 
-	   if (reliable_channels >= MIN_PATHS_REQUIRED)
-	     {
-	       std::thread(&passive_radar_cc::detector,this,input_items);
-	     }
-	   else
-	     {
-	       d_run_detector = false;
-	     }
-	}
-      
+				for (int i = processed;i < d_conv_chunk;i++)
+				  {
+				    d_resampled_input.get()[i] =0;
+				  }
+			      }
+
+			    memcpy(d_freq_shift_input.get(), d_resampled_input.get(), sizeof(gr_complex)*d_conv_chunk);
+			    // here correct  d_freq_shift_doppler by doppler shift
+			    // here perform  direct FFT for d_freq_shift_doppler
+			    // here perform  element-wise production d_freq_shift_doppler with FFT(input_items[ch])
+			    // here perform  Inverse FFT
+			    // here get magnitudes
+			  }
+		      }
+        
+		    d_run_detector = false;
+		  });
       // Tell runtime system how many output items we produced.
       return d_conv_chunk;
     }
